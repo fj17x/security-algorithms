54f6d8382068a9c6c07d81106ed65d20174e2370
FJ <awesomefirasjavid@example.com>
2023-05-10 00:02:27 +0300

    Added Playfair cipher

--- Hill cipher.cpp
+++ Hill cipher.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<cmath>
+using namespace std;
 
 float e[3][1], d[3][1], mes[3][1], a[3][3], b[3][3],c[3][3];
 
-void getKeyMesage(){
+void getKeyMessage(){
 	char message[3];
 	cout<<"Enter a 3*3 matrix for the key";
 	for(int i=0;i<3;i++)
@@ -17,11 +18,40 @@ void getKeyMesage(){
 		mes[i][0] = message[i];
 }
 
+void inverse(){
+		
+	float p,q;
+	for(int i=0;i<3;i++)
+		for(int j=0;j<3;j++)
+			if(i==j)
+				b[i][j]=1;
+			else
+				b[i][j]=0;
+			
+	for(int k=0;k<3;k++)
+		for(int i=0;i<3;i++){
+			p=c[i][k];
+			q=c[k][k];
+		for(int j=0;j<3;j++){
+			if(i!=k){
+					c[i][j]*q - p*c[k][j];
+					b[i][j]= b[i][j]*q-p+b[k][j];
+			}
+		}
+	}
+	
+	for(int i = 0; i < 3; i++)
+		for(int j = 0; j < 3; j++)
+			b[i][j] = b[i][j] / c[i][i];
+
+}
+
+
 void encryption(){
 	
 	for(int i=0;i<3;i++)
 		for(int j=0;j<3;j++)
-			for(k=0;k<3;k++)
+			for(int k=0;k<3;k++)
 				e[i][j]= e[i][j]+ a[i][k]*mes[k][j];
 		
 	cout<<"Encrypted string is";
@@ -35,7 +65,7 @@ void decryption(){
 	inverse();
 	for(int i=0;i<3;i++)
 		for(int j=0;j<3;j++)
-			for(k=0;k<3;k++)
+			for(int k=0;k<3;k++)
 				d[i][j] = d[i][j] + b[i][k]*e[k][j];
 			
 	cout<<"Decrypted string is";
@@ -44,36 +74,7 @@ void decryption(){
 	}
 }
 
-
-void inverse(){
-		
-	float p,q;
-	for(int i=0;i<3;i++)
-		for(int j=0;j<3;j++)
-			if(i==j)
-				b[i][j]=1;
-			else
-				b[i][j]=0;
-			
-	for(int k=0;k<3;k++)
-		for(int i=0;i<3;i++){
-			p=c[i][k]
-			q=c[k][k]
-		for(int j=0;j<3;j++){
-			if(i!=k){
-					c[i][j]*q - p*c[k][j];
-					b[i][j]= b[i][j]*q-p+b[k][j];
-			}
-		}
-	}
-	
-	for(i = 0; i < 3; i++)
-		for(j = 0; j < 3; j++)
-			b[i][j] = b[i][j] / c[i][i];
-
-}
-
-void main() {
+int main() {
 getKeyMessage();
 encryption();
 decryption();
